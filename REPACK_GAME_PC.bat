cdea7bf8e8267c31bd5659734804ae5f96119c1c
shimakazevn <shimakazevn@gmail.com>
2026-08-30 20:37:01 +0700

    Add repack tool and REPACK_GAME_PC.bat script

---
+++ REPACK_GAME_PC.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+title REPACK BẢN GAME TIẾNG VIỆT - HOME (ROOM) [PC]
+cls
+echo ============================================================
+echo   TỰ ĐỘNG REPACK APP.ASAR - TỰA GAME: HOME (ROOM) [PC]
+echo ============================================================
+echo.
+
+python tools\repack_pc_game.py
+
+echo.
+pause
